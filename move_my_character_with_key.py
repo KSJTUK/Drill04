d8e9a92cb0c05674e109e8a2dd535f8e31593331
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-09-25 23:52:50 +0900

    add animation setting function
    상태에 따라 애니메이션을 다르게 하는 함수 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -12,13 +12,14 @@ character_sonic = load_image('sonic.png')
 # 4, 5th y size 70 : 49 * 7 + 70 * 2 = 483
 
 def event_handle():
-	global running, dirX, dirY
+	global running, dirX, dirY, animation
 
 	events = get_events()
 	for event in events:
 		if event.type == SDL_QUIT:
 			running = False
 		elif event.type == SDL_KEYDOWN:
+			animation = "running"
 			if event.key == SDLK_ESCAPE:
 				running = False
 			elif event.key == SDLK_LEFT:
@@ -30,6 +31,7 @@ def event_handle():
 			elif event.key == SDLK_DOWN:
 				dirY -= 1
 		elif event.type == SDL_KEYUP:
+			animation = "idle"
 			if event.key == SDLK_LEFT:
 				dirX += 1
 			elif event.key == SDLK_RIGHT:
@@ -39,12 +41,27 @@ def event_handle():
 			elif event.key == SDLK_DOWN:
 				dirY += 1
 
-
-
 dirX, dirY = 0, 0
+frame_y = 434
+frame_size = 30, 49
+frame_cnt = 6
 frame = 0
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 sonic_size = 60, 100
+animation = "idle"
+
+# set sonic's animation
+def set_sonic_animation(animation):
+	global frame_size, frame_cnt, frame_y
+
+	if animation == "idle":
+		frame_size = 30, 49
+		frame_cnt = 6
+		frame_y = 434
+	elif animation == "running":
+		frame_size = 37, 49
+		frame_cnt = 9
+		frame_y = 385
 
 while running:
 	tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
@@ -55,12 +72,16 @@ while running:
 
 	# draw running animation
 	# running animation frame size: 37, 49, start pos: 0, 385(434 - 39)
-	character_sonic.clip_draw(frame * 37, 385, 37, 49, x, y, *sonic_size)
+	# character_sonic.clip_draw(frame * 37, 385, 37, 49, x, y, *sonic_size)
+
+	character_sonic.clip_draw(frame * frame_size[0], frame_y, *frame_size, x, y, *sonic_size)
 
 	update_canvas()
 	event_handle()
 
-	frame = (frame + 1) % 6
+	set_sonic_animation(animation)
+
+	frame = (frame + 1) % frame_cnt
 	x += dirX * 5
 	y += dirY * 5
 
