fa3909d486d449cbd9b1f9cbcf120519e163d985
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-09-25 22:22:10 +0900

    add exit loop case

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -10,7 +10,14 @@ character_sonic = load_image('sonic.png')
 # sprite size 805 x 483
 
 def event_handle():
-	pass
+	global running
+
+	events = get_events()
+	for event in events:
+		if event.type == SDL_QUIT:
+			running = False
+		elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+			running = False
 
 while running:
 	frame = 0
@@ -18,7 +25,7 @@ while running:
 	# idle frame size 30, 50, pos 0, 433
 	character_sonic.clip_draw(frame * 30, 433, 30, 50, TUK_WIDTH // 2, TUK_HEIGHT // 2, 50, 100)
 	update_canvas()
-	delay(1)
-	break
+	event_handle()
+	delay(0.05)
 
 close_canvas()
